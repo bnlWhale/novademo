1a346eeeb17c9671001bad2a416e024d0cdb66ba
bnl <ducounau@gmail.com>
2020-04-01 10:03:11 -0700

    add some tests

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import './App.css';
 import MainStage from "./components/container/MainStage";
+import MainContainer from "./components/container/MainContainer";
+import HomePage from "./components/container/HomePage";
+import "./components/cssdir/HomePageCss.css";
+
 
 function App() {
-  return (
-    <div className="App">
+    return (
+        <div className="App">
+            <div className="main_header">
+                <button onClick={()=>{
+                    alert("click me");
+                }}>click me</button>
+            </div>
+            <MainStage/>
 
-       <MainStage/>
-
-    </div>
-  );
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/utils/Utils.js
@@ -0,0 +1,63 @@
+import * as React from "react";
+
+const footerStyle = {
+    backgroundColor: "purple",
+    fontSize: "15px",
+    color: "white",
+    borderTop: "1px solid #E7E7E7",
+    textAlign: "center",
+    padding: "20px",
+    position: "fixed",
+    right: "0",
+    bottom: "0",
+    height: "50px",
+
+};
+
+const phantomStyle = {
+    display: "block",
+    padding: "20px",
+    height: "50px",
+
+};
+
+export function Footer({children}) {
+    return (
+        <div>
+            <div style={phantomStyle}/>
+            <div style={footerStyle}>{children}</div>
+        </div>
+    );
+}
+
+let pTxt =
+    ' It seems likely Apple will use Dark Sky\'s know how to bolster its own Weather app.\n' +
+    '        Apple has used data from Yahoo (which is owned by Engadget\'s parent company Verizon) and\n' +
+    '        The Weather Channel to power the app over the years. The website will stay online after then "in support of API\n' +
+    '        and iOS App customers." As for the API, it\'ll remain active until the end of next year, but Dark Sky won\'t let\n' +
+    '        anyone else sign up.'
+
+
+
+
+
+
+
+export function getTxtArray(num){
+
+    let a = Array(num).fill(null);
+
+    let b = a.map((val, index)=>{
+
+        let key = "getTxtArray "+index;
+        return (<p key={key}>
+            {pTxt+" "+index}
+        </p>);
+
+
+    })
+
+    return b;
+
+
+}
