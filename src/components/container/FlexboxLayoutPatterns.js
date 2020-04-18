952dfd6b2c87d71f231c2ae5c1ee96d36ec63b65
bnl <ducounau@gmail.com>
2020-04-18 00:36:59 -0700

    flexbox layout again

---
+++ src/components/container/FlexboxLayoutPatterns.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import '../cssdir/CssOnFlexLayout.css'
+import 'antd/dist/antd.css';
+import { Progress, Button } from 'antd';
+
+
+
+export default ()=>{
+
+    return(
+        <div className="container">
+            <div className="item"></div>
+            <div className="item"></div>
+            <div className="item"></div>
+        </div>
+
+    )
+}
