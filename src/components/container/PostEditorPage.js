4ef9e1f7704ec9f0124e1ef48c9ace292d8caa3a
bnl <ducounau@gmail.com>
2020-04-23 10:35:54 -0700

    modify css for editor

--- src/components/container/PostEditorPage.js
+++ src/components/container/PostEditorPage.js
@@ -3,6 +3,7 @@ import { EditorState, convertToRaw } from 'draft-js';
 import { Editor } from 'react-draft-wysiwyg';
 import draftToHtml from 'draftjs-to-html';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import '../cssdir/WYSIWYGComponentCss.css';
 
 
 class PostEditorPage extends Component {
@@ -16,21 +17,64 @@ class PostEditorPage extends Component {
         });
     };
 
+
+      uploadImageCallBack = (file)=> {
+        return new Promise(
+            (resolve, reject) => {
+                const xhr = new XMLHttpRequest();
+                xhr.open('POST', 'https://api.imgur.com/3/image');
+                xhr.setRequestHeader('Authorization', 'Client-ID XXXXX');
+                const data = new FormData();
+                data.append('image', file);
+                xhr.send(data);
+                xhr.addEventListener('load', () => {
+                    const response = JSON.parse(xhr.responseText);
+                    resolve(response);
+                });
+                xhr.addEventListener('error', () => {
+                    const error = JSON.parse(xhr.responseText);
+                    reject(error);
+                });
+            }
+        );
+    }
+
     render() {
         const { editorState } = this.state;
         return (
-            <div>
-                <Editor
-                    editorState={editorState}
-                    toolbarClassName="toolbarClassName"
-                    wrapperClassName="wrapperClassName"
-                    editorClassName="editorClassName"
-                    onEditorStateChange={this.onEditorStateChange}
-                />
-                <textarea
-                    disabled
-                    value={draftToHtml(convertToRaw(editorState.getCurrentContent()))}
-                />
+
+            <div  className="editor_container">
+
+                <div className="editor_sub_item">
+                    <Editor
+                        editorState={editorState}
+                        onEditorStateChange={this.onEditorStateChange}
+                        toolbar={{
+                            inline: { inDropdown: true },
+                            list: { inDropdown: true },
+                            textAlign: { inDropdown: true },
+                            link: { inDropdown: true },
+                            history: { inDropdown: true },
+                            image: { uploadCallback: this.uploadImageCallBack, alt: { present: true, mandatory: true } },
+                        }}
+                    />
+                    <textarea
+                        disabled
+                        value={draftToHtml(convertToRaw(editorState.getCurrentContent()))}
+                    />
+                </div>
+
+
+                <div className="editor_sub_item">
+
+                </div>
+
+
+                <div className="editor_sub_item_1">
+                    <button> <h1> submit </h1></button>
+                </div>
+
+
             </div>
         );
     }
